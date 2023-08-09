324469026487907e77dd500544dfc4a056d034c5
Deepak Kumar Singh <ks.deepak07@gmail.com>
2023-08-09 20:59:39 +0530

    first commit

---
+++ src/models/User.js
@@ -0,0 +1,58 @@
+const { Sequelize } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define("User", {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4,
+    },
+    firstname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        min: 6,
+      },
+    },
+    avatar: {
+      type: DataTypes.STRING,
+      defaultValue:
+        "https://res.cloudinary.com/domqfbn8t/image/upload/v1691570066/user3_i55cvk.png",
+    },
+    cover: {
+      type: DataTypes.STRING,
+      defaultValue:
+        "https://res.cloudinary.com/domqfbn8t/image/upload/v1691570985/blue-sky-and-puffy-clouds.jpg",
+    },
+    channelDescription: {
+      type: DataTypes.STRING,
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+  });
+};
